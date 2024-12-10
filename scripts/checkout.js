65c8130b9ae6d71d79ac4af539e2de4e261b7cff
Rala Shashank Yadav <ralashashankyadav0402@gmail.com>
2024-12-10 22:10:14 +0530

    Regenerat the HTML when updating delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -21,7 +21,7 @@ const deliveryDate = today.add(0,'minutes');
 console.log(deliveryDate.format('dddd, MMMM D, H:M'));
 // console.log(deliveryDate.format('dddd, MMMM D,H'));
 
-
+function renderOrderSummary(){
 let cartSummaryHTML = '';
 cart.forEach((cartItem)=>{
     const productId = cartItem.productId;
@@ -239,5 +239,9 @@ document.querySelectorAll('.js-delivery-option')
                             element.addEventListener('click',()=>{
                               const {productId, deliveryOptionId} = element.dataset;
                               updateDeliveryOption(productId,deliveryOptionId);
+                          renderOrderSummary();
                             });
                           });
+}            
+
+renderOrderSummary();
